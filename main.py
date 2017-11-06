c7db56339796b2de872da28d048de4d359f081ae
James Proctor <jamesproctor_Pitch@Jamess-MacBook-Pro.local>
2017-11-06 13:56:22 -0800

    changing port

--- main.py
+++ main.py
@@ -133,8 +133,7 @@ app = webapp2.WSGIApplication([
 
 def main():
 	from paste import httpserver
-	httpserver.serve(app, host='0.0.0.0', port='8080')
-	# httpserver.serve(app, host='127.0.0.1', port='8080')
+	httpserver.serve(app, host='0.0.0.0', port='18181')
 
 if __name__ == '__main__':
 	main()
